003bbbc650d3e01c2cac2aa7dc306c9500b9f8cc
Stefan Chan <stefan2846@hotmail.com>
2019-05-02 21:21:27 +0200

    Added a visual form for uploading recipes and table for showing recipes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,17 +1,54 @@
 import React, {Component} from 'react'
 import {DigitTable} from '@cthit/react-digit-components'
 import axios from 'axios'
+import './Home.css';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: []
+    };
+  //  this.getData();
+  }
+/*
+  getData() {
+    let self = this;
+    let url = "'localhost:4000';
+    axios.get(url)
+      .then(function(res) {
+        self.setState({
+
+        });
+      })
+      .catch(function() {
+        console.log("Database is not cooperating");
+      });
+  }
+  */
+
   render() {
-
-    const recipes = [];
-
     return (
-      <div>
+      <div className="recipetable">
         <DigitTable search
-                    titleText="Recept från IT-teknologer"
+                    titleText="Recept från diverse teknologer"
                     searchText="Sök efter recept"
+                    showSearchableProps
+                    idProp="id"
+                    startOrderBy="name"
+                    columnsOrder={["name", "score", "time"]}
+                    headerTexts={{
+                      name: "Recipe name",
+                      score: "Rating",
+                      time: "Time"
+                    }}
+                    data={[
+                      {
+                        name: "Dragonkyckling",
+                        score: "10",
+                        time: "50 min"
+                      }
+                    ]}
                     />
 
       </div>
--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -1,10 +1,64 @@
 import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import * as yup from 'yup'
+import {DigitText, DigitForm, DigitDesign,
+        DigitFormField, DigitTextField, DigitCheckbox,
+        DigitButton, DigitTextArea} from '@cthit/react-digit-components'
+import "./Upload.css"
 
 class Upload extends Component {
   render() {
     return (
-      <div>
-        <h1>Upload</h1>
+      <div className="upload">
+          <DigitText.Heading3 text="Ladda upp recept" />
+          <div className="uploadform">
+            <DigitForm
+              onSubmit={(values, actions) => {
+                console.log(values);
+              }}
+              initialValues={{ recipeName: "text", agreement: false }}
+              validationSchema={yup.object().shape({
+                text: yup.string().required("This can't be empty"),
+                agreement: yup.boolean().required("You need to accept")
+              })}
+              render={({ errors }) => (
+                <DigitDesign.Card absWidth="500px" absHeight="500px">
+                  <DigitDesign.CardBody>
+                    <DigitFormField
+                      name="recipeName"
+                      component={DigitTextField}
+                      componentProps={{
+                        upperLabel: "Receptnamn"
+                      }}
+                    />
+                    <DigitFormField
+                      name="recipeTime"
+                      component={DigitTextField}
+                      componentProps={{
+                        upperLabel: "Tid"
+                      }}
+                    />
+                    <DigitFormField
+                      name="recipeInstructions"
+                      component={DigitTextArea}
+                      componentProps={{
+                        upperLabel: "Receptinstruktioner",
+                        lowerLabel: "Fyll i instruktioner"
+                      }}
+                    />
+                    <DigitFormField
+                      name="agreement"
+                      component={DigitCheckbox}
+                      componentProps={{ primary: true, label:"Agreement" }}
+                    />
+                  </DigitDesign.CardBody>
+                  <DigitDesign.CardButtons>
+                    <DigitButton primary raised submit text="Lägg till recept" />
+                  </DigitDesign.CardButtons>
+                </DigitDesign.Card>
+              )}
+            />;
+        </div>
       </div>
     );
   }

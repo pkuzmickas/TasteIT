70b84648c4c897711d7df2f669051f10ac85d95e
Stefan Chan <stefan2846@hotmail.com>
2019-06-12 18:33:20 +0200

    widened and centralized the upload form, also added portions

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import * as yup from 'yup'
 import {DigitText, DigitForm, DigitDesign,
-        DigitFormField, DigitTextField, DigitCheckbox,
+        DigitFormField, DigitSelect,
+        DigitTextField, DigitCheckbox,
         DigitButton, DigitTextArea, DigitEditData,
         DigitToast, DigitToasActions} from '@cthit/react-digit-components'
 import "./Upload.css"
@@ -14,10 +15,13 @@ class Upload extends Component {
           <DigitText.Heading3 text="Upload recipe" />
           <div className="uploadform">
             <DigitEditData
+                marginVertical="10px"
+                absWidth="450px"
                 onSubmit={(values, actions) => {
                   // Send values to the backend to be stored in the database
                   console.log(values);
                   // Converts string into int
+                  values.recipeamount = parseInt(values.recipeamount, 10);
                   values.recipetime = parseInt(values.recipetime, 10);
                   // Consider sending a notifaction before reset
                   actions.resetForm();
@@ -25,46 +29,72 @@ class Upload extends Component {
                 validationSchema={yup.object().shape({
                   recipename: yup.string().required(),
                   recipetime: yup.string().required(),
+                  recipetime: yup.string().required(),
                   recipeingredients: yup.string().required(),
                   recipeinstructions: yup.string().required()
                 })}
                 submitText={"Upload recipe"}
-                keysOrder={["recipename", "recipetime", "recipeingredients",
-                            "recipedescription", "recipeinstructions"]}
+                keysOrder={["recipename", "recipetime", "recipeamount",
+                            "recipeingredients", "recipedescription",
+                            "recipeinstructions"]}
                 keysComponentData={{
                   recipename: {
                       component: DigitTextField,
                       componentProps: {
                           filled: true,
-                          upperLabel: "Recipename"
+                          upperLabel: "Recipename",
+                          lowerLabel: "Name of the recipe"
                       }
                   },
                   recipetime: {
                       component: DigitTextField,
                       componentProps: {
                         filled: true,
-                        upperLabel: "Recipetime"
+                        upperLabel: "Recipetime",
+                        lowerLabel: "Cooking time"
                       }
+                  },
+                  recipeamount: {
+                    component: DigitSelect,
+                    componentProps: {
+                      lowerLabel: "Amount of portions",
+                      valueToTextMap: {
+                        1: "1",
+                        2: "2",
+                        3: "3",
+                        4: "4",
+                        5: "5",
+                        6: "6",
+                        7: "7",
+                        8: "8",
+                        9: "9",
+                        10: "10"
+                      },
+                      filled: true
+                    }
                   },
                   recipeingredients: {
                       component: DigitTextArea,
                       componentProps: {
                         filled: true,
-                        upperLabel: "Recipe Ingredients"
+                        upperLabel: "Recipe Ingredients",
+                        lowerLabel: "The ingredients to use"
                       }
                   },
                   recipedescription: {
                       component: DigitTextArea,
                       componentProps: {
                         filled: true,
-                        upperLabel: "Recipedescription"
+                        upperLabel: "Recipedescription",
+                        lowerLabel: "Description of the recipe (Optional)"
                       }
                   },
                   recipeinstructions: {
                       component: DigitTextArea,
                       componentProps: {
                         filled: true,
-                        upperLabel: "Recipe Instructions"
+                        upperLabel: "Recipe Instructions",
+                        lowerLabel: "Instruction in how to cook the recipe"
                       }
                   }
                 }}

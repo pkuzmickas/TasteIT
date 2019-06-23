bc9ac323a165b8b31f428c079a169002c24114ac
Stefan Chan <stefan2846@hotmail.com>
2019-06-23 17:09:04 +0200

    Preliminary display page for recipe

--- frontend/src/components/elements/recipe/Ingredientlist.js
+++ frontend/src/components/elements/recipe/Ingredientlist.js
@@ -1,24 +1,40 @@
 import React, {Component} from 'react'
-import {DigitText} from '@cthit/react-digit-components'
+import {DigitText, DigitCheckbox, DigitLayout} from '@cthit/react-digit-components'
 import './IngredientList.css'
 
 class Ingredientlist extends Component {
-  createIngredients(ingredient) {
-    // ingredient should be a string
-    return <li key={ingredient.key}>
-        <DigitText.Subtitle2 white='true' text={ingredient} />
-          </li>
+
+  createIngredients = item => {
+    // Replace with JSON
+    let text = item[1] + " " + item[2] + " " + item[0];
+    console.log(text);
+    return <div className="ingredientListItem">
+                <DigitCheckbox label={text}
+                               primary />
+           </div>
   }
 
   render() {
     // ingredients should be an array of strings
     var ingredientList = this.props.ingredients;
+    console.log(ingredientList);
     var listItems = ingredientList.map(this.createIngredients);
 
     return (
+      /*
       <ul className="ingredientList">
         {listItems}
       </ul>
+      */
+      <div className="ingredientListArea">
+        <DigitLayout.UniformGrid minItemWidth="240px"
+                                 minItemHeight="78px"
+                                 rowGap="20px"
+                                 columnGap="20px">
+              {listItems}
+        </DigitLayout.UniformGrid>
+
+      </div>
     );
   }
 }
--- frontend/src/components/elements/recipe/Instructions.js
+++ frontend/src/components/elements/recipe/Instructions.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react'
 import {DigitText} from '@cthit/react-digit-components'
+import './Instructions.css'
 
 class Instructions extends Component {
 
   render() {
+    console.log(this.props.instructions);
     return (
-      <div>
+      <div className="recipeInstructionsArea">
+        <DigitText.Text text={this.props.instructions} />
       </div>
     );
   }
